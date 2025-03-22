3ecf07ad46969ac5482ed47b7df851af0d513856
Innovative321 <138666055+Innovative321@users.noreply.github.com>
2025-03-22 15:13:25 +0530

    Update QuizappApplicationTests.java

--- src/test/java/ia/quizapp/QuizappApplicationTests.java
+++ src/test/java/ia/quizapp/QuizappApplicationTests.java
@@ -3,7 +3,7 @@ package ia.quizapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @RunWith(SpringRunner.class)
 class QuizappApplicationTests {
 
